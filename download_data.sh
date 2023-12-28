a46f3e022cdd91a05c3958b511b98276ba51c6c1
cima.uba-paola.corrales <paola.corrales@cima.fcen.uba.ar>
2023-12-28 16:14:19 -0300

    fix download

--- download_data.sh
+++ download_data.sh
@@ -4,9 +4,9 @@
 # They can also be downloaded manually from https://zenodo.org/records/10439645
 
 # OBS
-wget https://zenodo.org/records/10439645/files/OBS.tar.gz?download=1
+wget -O OBS.tar.gz https://zenodo.org/records/10439645/files/OBS.tar.gz?download=1
 tar -xvf OBS.tar.gz
 
 # GUESS
-wget https://zenodo.org/records/10439645/files/GUESS.tar.gz?download=1
+wget -O GUESS.tar.gz https://zenodo.org/records/10439645/files/GUESS.tar.gz?download=1
 tar -xvf GUESS.tar.gz
